2fdf071c84889c97a327f04ebdd2a4b4f0da6983
Rafael Estevam <rafael.estevam@gauge.com.br>
2020-11-16 22:04:27 -0300

    ajust backend

--- back/dataUtils.py
+++ back/dataUtils.py
@@ -35,7 +35,6 @@ def getCSVByState(request):
     json_data = request.get_json()
     
     queryString = 'DS_CARGO == "' + json_data['cargo'] + '" & NM_UE == "' + json_data['cidade'] + '"'
-    # csvString = "data/candidaturas/consulta_cand_2020_TESTE.csv"
     csvString = "data/candidaturas/consulta_cand_2020_"+ json_data['estado'] +".csv"
 
     data = pd.read_csv(csvString, sep=";", encoding="ISO-8859-1",skiprows=0)
